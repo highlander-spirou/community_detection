d8cb8e8bc52e4eabffa476507a51bb6b8534ac3a
shyn_le <highlandetspirou2812@gmail.com>
2023-10-13 01:44:14 +0700

    lại :(

--- hh/db/__init__.py
+++ hh/db/__init__.py
@@ -8,8 +8,9 @@ load_dotenv('.env')
 URI = os.environ.get('PG_URI')
 
 
-async def write_log(log_message, status, connection):
-    async with connection.cursor() as acur:
-        await acur.execute(
-            "INSERT INTO logs (log_message, status) VALUES (%s, %s)",
-            (log_message, status))
+async def write_log(log_message, status):
+    async with await psycopg.AsyncConnection.connect(URI) as connection:
+        async with connection.cursor() as cursor:
+            await cursor.execute(
+                "INSERT INTO logs (log_message, status) VALUES (%s, %s)",
+                (log_message, status))
--- index.py
+++ index.py
@@ -11,22 +11,21 @@ async def main():
     metadata = await get_meta_data()
     OFFSET = metadata['CHECKPOINT']
     current_page = 0
-    async with await psycopg.AsyncConnection.connect(URI) as aconn:
-        while current_page < 351:  # có 70k data, mỗi page lấy 200 data => có 350 page
-            current_page += 1
-            pmid_list = await get_pmid_list(LIMIT, OFFSET)
-            results = []
-            for i in pmid_list:
-                try:
-                    print(f'Đang xử lí {i}')
-                    result = await get_pmid_info(i)
-                    results.append((i, result))
-                    await write_log(f'Fetch {i} success', 'success', aconn)
-                    OFFSET += 1
-                    await update_meta_data(OFFSET)
-                except Exception:
-                    await write_log(f'Fail to {i}', 'fail', aconn)
-            pickle_utils.save_pickle(results)
+    while current_page < 351:  # có 70k data, mỗi page lấy 200 data => có 350 page
+        current_page += 1
+        pmid_list = await get_pmid_list(LIMIT, OFFSET)
+        results = []
+        for i in pmid_list:
+            try:
+                print(f'Đang xử lí {i}')
+                result = await get_pmid_info(i)
+                results.append((i, result))
+                await write_log(f'Fetch {i} success', 'success')
+                OFFSET += 1
+                await update_meta_data(OFFSET)
+            except Exception:
+                await write_log(f'Fail to {i}', 'fail')
+        pickle_utils.save_pickle(results)
 
 if __name__ == '__main__':
     if sys.platform == "win32":
