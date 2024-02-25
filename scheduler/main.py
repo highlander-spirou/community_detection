4df294ac62eb1196bfdbc36ed3fd01bafba5e347
highlander-spirou <highlandetspirou2812@gmail.com>
2024-02-25 18:30:09 +0700

    sync: thinkpad@2024-02-25

--- scheduler/main.py
+++ scheduler/main.py
@@ -1,14 +1,33 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-import time
+# import time
+# from random import randint
 
 def my_task():
   print("Task executed!")
 
-def main():
-  scheduler = BackgroundScheduler()
-  scheduler.add_job(my_task, 'interval', seconds=2)  # Run every 10 seconds
+scheduler = BackgroundScheduler()
+
+def run_scheduler():
+  scheduler.add_job(my_task, 'interval', seconds=2, id='1')  # Run every 10 seconds
   print("Scheduler started...")
   scheduler.start()
 
 if __name__ == "__main__":
-  main()
+  from time import sleep
+
+  graph = """
+check_redis_queue > get_params,
+get_params > get_pmid_list,
+get_pmid_list > task_publisher,
+get_pmid_list > update_params
+"""
+
+  run_scheduler()
+  i = 0
+  while i <= 7:
+    sleep(4)
+    print("Starting scheduler at main thread")
+    i += 1
+  else:
+    scheduler.remove_job('1')
+
---
+++ scheduler/tasks.py
@@ -0,0 +1,14 @@
+def check_redis_queue():
+    pass
+
+def get_params():
+    pass
+
+def get_pmid_list():
+    pass
+
+def task_publisher():
+    pass
+
+def update_params():
+    pass
